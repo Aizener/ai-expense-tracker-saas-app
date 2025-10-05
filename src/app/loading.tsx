253a23cd0ed6250e801fb2b541ff7dfc70be4cdd
Cola. <1215627787@qq.com>
2025-10-05 14:42:26 +0800

    feat: add loading.

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+function Loading() {
+  return (
+    <div
+      className="mt-8 mx-auto flex justify-center items-center px-30 py-16 text-lg font-bold bg-gradient-to-r from-blue-500 via-sky-500 to-teal-400 rounded-md shadow-xl text-foreground text-white"
+    >
+      加载中...
+    </div>
+  );
+}
+
+export default Loading;
